73afaa09c11fbeed86188cc2ec565970487e1556
HaykZiroyan <haik.ziroyan@mail.ru>
2021-07-18 23:39:31 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,307 @@
+var cardInfo=[
+    [
+        {key: 1, iamge: 'images/Picture1.png', name: 'Classic 600', price: 400, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 600 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER Զառեր՝ 5 հատ '},
+        {key: 2, iamge: 'images/Picture2.png', name: 'Classic 1000', price: 600, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 1000 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER, BIG BLIND, SMALL BLIND Զառեր՝ 6 հատ'},
+        {key: 3, iamge: 'images/Picture1.png', name: 'Classic 600', price: 400, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 600 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER Զառեր՝ 5 հատ '},
+        {key: 4, iamge: 'images/Picture2.png', name: 'Classic 1000', price: 600, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 1000 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER, BIG BLIND, SMALL BLIND Զառեր՝ 6 հատ'},
+        {key: 5, iamge: 'images/Picture1.png', name: 'Classic 600', price: 400, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 600 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER Զառեր՝ 5 հատ '}
+    ],
+    [
+        {key: 6, iamge: 'images/Picture1.png', name: 'Classic 600', price: 400, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 600 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER Զառեր՝ 5 հատ '},
+        {key: 7, iamge: 'images/Picture2.png', name: 'Classic 1000', price: 600, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 1000 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER, BIG BLIND, SMALL BLIND Զառեր՝ 6 հատ'},
+        {key: 8, iamge: 'images/Picture1.png', name: 'Classic 600', price: 400, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 600 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER Զառեր՝ 5 հատ '},
+        {key: 9, iamge: 'images/Picture2.png', name: 'Classic 1000', price: 600, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 1000 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER, BIG BLIND, SMALL BLIND Զառեր՝ 6 հատ'},
+        {key: 10, iamge: 'images/Picture1.png', name: 'Classic 600', price: 400, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 600 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER Զառեր՝ 5 հատ '}
+    ],
+    [
+        {key: 11, iamge: 'images/Picture1.png', name: 'Classic 600', price: 400, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 600 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER Զառեր՝ 5 հատ '},
+        {key: 12, iamge: 'images/Picture2.png', name: 'Classic 1000', price: 600, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 1000 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER, BIG BLIND, SMALL BLIND Զառեր՝ 6 հատ'},
+        {key: 13, iamge: 'images/Picture1.png', name: 'Classic 600', price: 400, info: 'Քեյսի չափսը՝ սմ Տարողունակությունը՝ 600 ֆիշկա Ֆիշկայի տեսակը՝ Խաղաքարտեր՝ Club Special N92 (2 տուփ) Աքսեսուարներ՝ DEALER Զառեր՝ 5 հատ '}
+    ]
+];
+let num = 0;
+let k = 0;
+let obshi = 0;
+function getNum() {
+    var tag = document.createElement("p");
+    var text = document.createTextNode(num);
+    tag.classList.add("number");
+    tag.setAttribute("id", text)
+    tag.appendChild(text);
+    var element = document.getElementById("New");
+    
+        element.innerHTML = "";
+        element.appendChild(tag);  
+}
+
+function popox() {
+    let addobshi = document.createElement('p');
+    addobshi.textContent = obshi;
+    document.getElementById('obshi1').innerHTML = "";
+    document.getElementById('obshi1').appendChild(addobshi);
+}
+
+for (i in cardInfo) {
+    let middiv = document.createElement('div');
+    middiv.classList.add('cont')
+    document.getElementById('card').appendChild(middiv);
+    for (j in cardInfo[i]) {
+        let inpnum = 1;
+        let carddiv = document.createElement('div');
+        carddiv.classList.add('card');
+        middiv.appendChild(carddiv);
+        
+        var img1 = document.createElement("img");
+        img1.src = cardInfo[i][j].iamge;
+        carddiv.append(img1);
+
+        h2 = document.createElement("h3");
+        h2.textContent =cardInfo[i][j].name;
+        carddiv.append(h2);
+
+        let text = document.createTextNode(cardInfo[i][j].info);
+        carddiv.append(text);
+
+        let value = document.createElement("h2");
+        value.textContent = cardInfo[i][j].price + "դր";
+        value.classList.add('price');
+        carddiv.appendChild(value);
+
+        let min = document.createElement("button");
+        min.innerHTML = "-"
+        min.classList.add("plus-min");
+        min.onclick = function() {
+            inpnum = inpnum - 1;
+            input.value = inpnum;
+        }
+        carddiv.append(min);
+
+        let input = document.createElement("input");
+        input.value = inpnum;
+        carddiv.append(input);
+        
+
+        let plus = document.createElement("button");
+        plus.innerHTML = "+"
+        plus.classList.add("plus-min");
+        plus.onclick = function() {
+            inpnum = inpnum + 1;
+            input.value = inpnum;
+        }
+        carddiv.append(plus);
+
+        let member = [i, j];
+        let btn = document.createElement("button");
+        btn.innerHTML = "add to busket";
+        btn.classList.add("set-busket");
+
+        btn.onclick = function() {
+            num = num + 1; 
+            getNum();
+            
+            let ul = document.getElementById('ul')
+            var li=document.createElement('li');
+            ul.appendChild(li);
+
+            let pic = document.createElement('img');
+            pic.src = cardInfo[member[0]][member[1]].iamge;
+            pic.classList.add('pic')
+            li.appendChild(pic);
+
+            let name = document.createElement("p");
+            name.classList.add('name');
+            name.textContent =cardInfo[member[0]][member[1]].name;
+            li.appendChild(name);
+
+            let gin = document.createElement("p");
+            gin.classList.add("gin")
+            gin.textContent =cardInfo[member[0]][member[1]].price;
+            li.appendChild(gin);
+
+            let qan = document.createElement("p");
+            qan.classList.add("qan");
+            qan.textContent = inpnum;
+            li.appendChild(qan);
+
+            let yndh = document.createElement("p");
+            yndh.classList.add("last");
+            yndh.textContent = inpnum*cardInfo[member[0]][member[1]].price;
+            li.appendChild(yndh);
+
+            let remove = document.createElement("button");
+            remove.innerHTML =  "remove";
+            remove.onclick = function() {
+                let change = this.parentNode.childNodes[4].innerHTML;
+                obshi = obshi - change;
+                popox();
+                li.remove();
+                num = num - 1;
+                getNum();
+            }
+            li.append(remove);
+
+            obshi = obshi + inpnum*cardInfo[member[0]][member[1]].price;
+            popox();
+            
+        } //input-ից թիվ ստանալ
+        carddiv.append(btn);
+        
+    }
+}
+
+var a = 1
+function newBox() {
+    let el = document.getElementById('para-1');
+    let remScroll = document.getElementById('kar');
+    if (a == 1) {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+        remScroll.classList.remove('scrollKar');
+        el.classList.remove('none');
+        el.classList.add('buy');
+        a = a + 1;
+
+        // let changeColor = document.getElementById('ul');
+        // var c = changeColor.childElementCount;
+        // console.log(c)
+        // for (let n = 1; n < c/2; n++) {
+        
+        //     changeColor.nth-child(2).classList.add('defcolor');
+        // }
+    } else {
+        remScroll.classList.add('scrollKar');
+        el.classList.remove('buy');
+        el.classList.add('none');
+        a = a - 1;
+    }
+}
+function closeBox() {
+    let el = document.getElementById('para-1');
+    document.getElementById('kar').classList.add('scrollKar');
+    el.classList.remove('buy');
+    el.classList.add('none');
+    a = a - 1;
+}
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // for (i in cardInfo) {
+    //     let inpnum = 1;
+    //     let div1 = document.createElement('div');
+    //     div1.classList.add('card');
+
+    //     var img1 = document.createElement("img");
+    //     img1.src = cardInfo[i].iamge;
+    //     div1.append(img1);
+
+    //     h2 = document.createElement("h2");
+    //     h2.textContent =cardInfo[i].name;
+    //     div1.append(h2);
+
+    //     let text = document.createTextNode(cardInfo[0].info);
+    //     div1.append(text);
+
+    //     div1.append(document.createElement("br"));
+
+    //     let min = document.createElement("button");
+    //     min.innerHTML = "-"
+    //     min.classList.add("plus-min");
+    //     min.onclick = function() {
+    //         inpnum = inpnum - 1;
+    //         input.value = inpnum;
+    //     }
+    //     div1.append(min);
+
+    //     let input = document.createElement("input");
+    //     input.setAttribute("id", i);
+    //     input.value = inpnum;
+    //     div1.append(input);
+
+    //     let plus = document.createElement("button");
+    //     plus.innerHTML = "+"
+    //     plus.classList.add("plus-min");
+    //     plus.onclick = function() {
+    //         inpnum = inpnum + 1;
+    //         input.value = inpnum;
+    //     }
+    //     div1.append(plus);
+
+        
+
+    //     let btn = document.createElement("button");
+    //     btn.innerHTML = "add to busket";
+    //     btn.setAttribute("id", i)
+    //     btn.classList.add("set-busket");
+    //     btn.onclick = function() {
+    //         let id = this.id;
+                        
+    //         num = num + 1; 
+    //         var tag = document.createElement("p");
+    //         var text = document.createTextNode(num);
+    //         tag.classList.add("number");
+    //         tag.setAttribute("id", text)
+    //         tag.appendChild(text);
+    //         var element = document.getElementById("New");
+    //         if (num == 1) {
+    //             element.appendChild(tag);
+    //         } else {
+    //             element.innerHTML = "";
+    //             element.appendChild(tag);
+    //         }
+            
+    //         var li=document.createElement('li');
+
+            // ul.appendChild(li);
+            // li.innerHTML=cardInfo[id].name +" "+ inpnum + " hat";
+            // let remove = document.createElement("button");
+            // remove.innerHTML =  "remove";
+            // el.append(remove);
+        
+
+    //     } //input-ից թիվ ստանալ
+    //     div1.append(btn);
+        
+    //     div.appendChild(div1);
+    // }
+
+
+
+    // var a = 1
+    // function newBox() {
+        
+    //     if (a == 1) {
+    //         el.classList.remove('none');
+    //         el.classList.add('buy');
+    //         a = a + 1;
+    //     } else {
+    //         el.classList.remove('buy');
+    //         el.classList.add('none');
+    //         a = a - 1;
+    //     }
+    // }  
